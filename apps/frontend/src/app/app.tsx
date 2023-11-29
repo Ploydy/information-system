b504eb79d3a0b0bd668dad8a9bdb96690f1d2d4e
Ploydy <Lfecarballo20@gmail.com>
2023-11-29 14:45:53 +0800

    initail commit

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
+      <h1>hello </h1>
     </div>
   );
 }
