bc5b631ff4ec17a833994896df4ee5372ee4e349
Ploydy <Lfecarballo20@gmail.com>
2023-11-29 17:57:27 +0800

    header nav setup

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import {
+  Button,
+  Card,
+  CardBody,
+  Checkbox,
+  Input,
+} from '@nextui-org/react';
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  
 
-export default Login
+  const handleLogin = () => {
+    console.log('Login')
+  }
+ 
+  return (
+    <div>
+      <div className="px-4 py-40 mx-auto sm:px-6 lg:px-8 max-w-7x1">
+        <div className="max-w-lg mx-auto text-center">
+          <Card
+            className="pt-4 pb-4 px-8 bg-transparent"
+            style={{ border: 'solid' }}
+          >
+            <h1 className="text-tiny uppercase font-bold">Login</h1>
+            <CardBody>
+              <Input
+                className="py-1"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+
+              <Input
+                className="py-1 "
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <Checkbox className="py-4">Remember me</Checkbox>
+              <div className="mx-0 my-2">
+                {/* <div>
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Button variant="bordered" className="capitalize">
+                        
+                      </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                      aria-label="Single selection example"
+                      variant="flat"
+                      disallowEmptySelection
+                      selectionMode="single"
+                      selectedKeys={selectedKeys}
+                      onSelectionChange={setSelectedKeys}
+                      {...props}
+                    >
+                      <DropdownItem key="admin">Admin</DropdownItem>
+                      <DropdownItem key="employee">Employee</DropdownItem>
+                      <DropdownItem key="secretary">Secretary</DropdownItem>
+                      <DropdownItem key="customer">Customer</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </div> */}
+                <div className="my-2">
+                  
+                    <Button>Create an account</Button>
+                  
+                </div>
+              </div>
+              <Button onClick={handleLogin}>Login</Button>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- apps/frontend/src/pages/admin/layout/Header.tsx
+++ apps/frontend/src/pages/admin/layout/Header.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-export default Header
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  
+  const handleLogout = () => {
+    console.log(`Logout`)
+    
+  };
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-slate-500">
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+          {/* <link href="/Admin"> */}
+            <p className="font-bold text-inherit">UniPrime</p>
+            {/* </link> */}
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            {/* <Link color="foreground" href="/Admin/Overview"> */}
+            Overview
+            {/* </Link> */}
+          </NavbarItem>
+          <NavbarItem>
+            {/* <Link color="foreground" href="/Admin" > */}
+            Dashboard
+            {/* </Link> */}
+          </NavbarItem>
+          <NavbarItem>
+           {/*  <Link color="foreground" href="/Admin/Users"> */}
+            Users
+            {/* </Link> */}
+          </NavbarItem>
+          <NavbarItem>
+            {/* <Link color="foreground" href="/Admin/Email"> */}
+            Email
+            {/* </Link> */}
+          </NavbarItem>
+          <NavbarItem>
+            {/* <Link color="foreground" href="/Admin/Feedback"> */}
+            Feedback
+            {/* </Link> */}
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button onClick={handleLogout} variant="flat">
+              Logout
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              {/* <link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === menuItems.length - 1
+                    ? "danger"
+                    : "foreground"
+                }
+                className="w-full"
+                href="#"
+                size="lg"
+              >
+                {item}
+              </link> */}
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+  );
+};
+
+export default Header;
