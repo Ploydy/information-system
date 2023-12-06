90057a0371fbf688947567276feff23350b4eece
Ploydy <Lfecarballo20@gmail.com>
2023-12-06 15:15:57 +0800

    nestjs and mongo db ready

--- apps/backend2/src/app/app.module.ts
+++ apps/backend2/src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from '../cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/primeunilumDB'), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+/* 
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/primeunilumDB', {
+      connectionName: 'cats',
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/primeunilumDB', {
+      connectionName: 'Users',
+    }),
+  ],
+  
+})
+export class AppModule {} 
+*/
--- apps/frontend/src/Views/Component/Sidebar.tsx
+++ apps/frontend/src/Views/Component/Sidebar.tsx
@@ -44,7 +44,7 @@ const Sidebar = () => {
           </li>
 
           <li>
-            <Link to="/Tickets"
+            <Link to="/admin/Tickets"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg
@@ -61,7 +61,7 @@ const Sidebar = () => {
           </li>
 
           <li>
-            <Link to="/Users"
+            <Link to="/Admin/User"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <svg
