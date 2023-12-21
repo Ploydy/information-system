4ce985249673784a4902e30ff18f428382c87f82
Ploydy <Lfecarballo20@gmail.com>
2023-12-21 10:22:28 +0800

    crud for products working properly

--- apps/frontend/src/Views/Admin/ProductCard.tsx
+++ apps/frontend/src/Views/Admin/ProductCard.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -6,9 +7,11 @@ import {
   Image,
 } from '@nextui-org/react';
 /* import aluminum from '../../Gallery/Products/aluminum.jpg'; */
-import glass from '../../Gallery/Products/glass.jpg'
+import glass from '../../Gallery/Products/glass.jpg';
 import { useEffect, useState } from 'react';
 import { ProductResponse } from '@information-system/mylib';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
   const [products, setProducts] = useState<ProductResponse[]>([]);
@@ -30,36 +33,58 @@ const ProductCard = () => {
     })();
   }, []);
 
+  const handleDelete = async (id: string) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:4200/api/products/${id}`
+      );
+      if (response.status === 200) {
+        getProducts();
+      } else {
+        console.error(`Failed to delete user with ID ${id}.`);
+      }
+    } catch (error) {
+      console.error(`Error deleting user with ID ${id}:`, error);
+    }
+  };
+
+  const handleUpdate = async (id: string) => {};
+
   return (
     <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
       {products.map((product) => (
-        <Card
-          key={product.category}
-          className=""
-          shadow="sm"
-          isPressable
-          onPress={() => console.log('Modal (add to cart or nah)')}
-        >
-          <CardHeader>
-            <p>{product.category}</p>
-          </CardHeader>
-          <CardBody className="overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={product.title}
-              className="w-full object-cover h-[140px]"
-              src={glass}
-              
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{product.title}</b>
-            <p className="text-default-500">${product.price}</p>
-            <p className="text-default-500">{product.description}</p>
-          </CardFooter>
-        </Card>
+        <Link to={`/admin/products/${product._id}`} >
+          <Card
+            key={product._id}
+            className=""
+            shadow="sm"
+            isPressable
+            onPress={() => handleUpdate(product._id)}
+          >
+            <CardHeader>
+              <p>{product.category}</p>
+            </CardHeader>
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={product.title}
+                className="w-full object-cover h-[140px]"
+                src={glass}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{product.title}</b>
+              <p className="text-default-500">${product.price}</p>
+              <p className="text-default-500">{product.description}</p>
+              <Button onClick={() => handleDelete(product._id)}>
+                {' '}
+                Delete{' '}
+              </Button>
+            </CardFooter>
+          </Card>
+        </Link>
       ))}
     </div>
   );
